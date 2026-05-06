c43a69091159737fbe18b2a6d7a1d786e69b0bf4
Technologicaljerry <cserajatverma@gmail.com>
2026-05-06 23:57:23 +0530

    feat: implement authentication system with login, registration, and protected routes using Tailwind components

---
+++ imports/api/users/server/publications.ts
@@ -0,0 +1,14 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.publish('userData', function () {
+  if (this.userId) {
+    return Meteor.users.find(
+      { _id: this.userId },
+      {
+        fields: { profile: 1, emails: 1 },
+      }
+    );
+  } else {
+    this.ready();
+  }
+});
